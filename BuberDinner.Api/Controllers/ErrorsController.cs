b129076cbbe20de718abc456fc0c49a283bae132
Emirhan <emirhancifci@hotmail.com>
2023-08-07 01:29:57 +0300

    ZEC-000 feat: added duplicate email exception

--- BuberDinner.Api/Controllers/ErrorsController.cs
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -1,3 +1,4 @@
+using BuberDinner.Application.Common.Errors;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,13 @@ public class ErrorsController : ControllerBase
     public IActionResult Error()
     {
         var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-        
 
-
-        return Problem(title: exception?.Message, statusCode: 400);
+        var (statusCode, message) = exception switch
+        {
+            IServiceException serviceException => ((int)serviceException.StatusCode,serviceException.ErrorMessage),
+            _ => (StatusCodes.Status500InternalServerError, "An unexpected error occurred")
+        };
+        return Problem(statusCode: statusCode, title: message);
     }
 
 }
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,5 @@
 
-using BuberDinner.Api.Errors;
+using BuberDinner.Api.Common.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
@@ -24,15 +24,17 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
     // app.UseMiddleware<ErrorHandlingMiddleware>();
+
     app.UseExceptionHandler("/error");
 
-    app.Map("/error", (HttpContext httpContext) => {
+    /* app.Map("/error", (HttpContext httpContext) => {
         var exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
 
         return Results.Problem();
         
 
-    });
+    }); */
+
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
